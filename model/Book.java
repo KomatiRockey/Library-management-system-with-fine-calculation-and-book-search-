a7f3bf913686323790b3df04b3e5ef824e5ea40f
KOMATI RAKESH <rrocky87221@gmail.com>
2025-08-05 21:18:26 +0530

    Update Book.java

--- model/Book.java
+++ model/Book.java
@@ -1,1 +1,19 @@
+package com.library.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int year;
+    private String genre;
+    private boolean available = true;
+
+    // Getters and Setters
+}
 
