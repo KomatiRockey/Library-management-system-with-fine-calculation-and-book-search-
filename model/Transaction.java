5f93c44fd4a18563c9f9294a5503ee67dfb0de09
KOMATI RAKESH <rrocky87221@gmail.com>
2025-08-05 21:18:54 +0530

    Update Transaction.java

--- model/Transaction.java
+++ model/Transaction.java
@@ -1,1 +1,24 @@
+package com.library.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Book book;
+
+    private Date borrowDate;
+    private Date returnDate;
+    private int fine;
+
+    // Getters and Setters
+}
 
