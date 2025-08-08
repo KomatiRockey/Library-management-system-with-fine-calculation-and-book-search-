81c72f517c5427f9881ebc65baf2f71b02a6ebf8
KOMATI RAKESH <rrocky87221@gmail.com>
2025-08-08 18:09:04 +0530

    Update transactionrepository.java

--- repository/transactionrepository.java
+++ repository/transactionrepository.java
@@ -1,6 +1,9 @@
-package com.library.repository;
+package com.example.library.repository;
 
-import com.library.model.Transaction;
+import com.example.library.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserId(Long userId);
+}
