157927f1ec56b3898ce506440b4d7eba3126a657
KOMATI RAKESH <rrocky87221@gmail.com>
2025-08-08 18:07:01 +0530

    Update Transaction.java

--- model/Transaction.java
+++ model/Transaction.java
@@ -1,24 +1,32 @@
-package com.library.model;
+package com.example.library.model;
 
-import javax.persistence.*;
-import java.util.Date;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private User user;
+    @ManyToOne private User user;
+    @ManyToOne private Book book;
 
-    @ManyToOne
-    private Book book;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+    private int fine; // in currency units (e.g., cents or rupees)
 
-    private Date borrowDate;
-    private Date returnDate;
-    private int fine;
+    public Transaction() {}
 
-    // Getters and Setters
+    // Getters & setters
+    public Long getId() { return id; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+    public Book getBook() { return book; }
+    public void setBook(Book book) { this.book = book; }
+    public LocalDate getBorrowDate() { return borrowDate; }
+    public void setBorrowDate(LocalDate borrowDate) { this.borrowDate = borrowDate; }
+    public LocalDate getReturnDate() { return returnDate; }
+    public void setReturnDate(LocalDate returnDate) { this.returnDate = returnDate; }
+    public int getFine() { return fine; }
+    public void setFine(int fine) { this.fine = fine; }
 }
-
