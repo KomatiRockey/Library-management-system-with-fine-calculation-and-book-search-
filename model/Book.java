c1fd013e4588c312b151047a266549e01b6a84d6
KOMATI RAKESH <rrocky87221@gmail.com>
2025-08-08 18:05:47 +0530

    Update Book.java

--- model/Book.java
+++ model/Book.java
@@ -1,11 +1,10 @@
-package com.library.model;
+package com.example.library.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -14,6 +13,26 @@ public class Book {
     private String genre;
     private boolean available = true;
 
-    // Getters and Setters
-}
+    // Constructors
+    public Book() {}
+    public Book(String title, String author, int year, String genre) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.available = true;
+    }
 
+    // Getters / Setters
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+}
